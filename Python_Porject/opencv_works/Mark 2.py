bb7b8e99999fb9caf6a0069f1ac092d512972f1f
Tony Tree <cuihongwei2009@126.com>
2018-10-25 13:24:58 +0800

    Porject

--- Python_Porject/opencv_works/Mark 2.py
+++ Python_Porject/opencv_works/Mark 2.py
@@ -5,12 +5,9 @@ cap = cv.VideoCapture(0)
 
 while (True):
     ret,frame = cap.read()
-    ret = cap.set(cv.CAP_PROP_FRAME_WIDTH,640)
-    ret = cap.set(cv.CAP_PROP_FRAME_HEIGHT,480)
     gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
     cv.imshow('frame',gray)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
-
 cap.release()
 cv.destoryAllWindows()
